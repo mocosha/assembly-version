e9a12f34a29aad762a642e00bc0d162977465324
milosstojanovic <milos.stojanovic@mocosha.com>
2018-11-15 11:08:12 +0100

    Added build scripts. Implemented FAKE 5

---
+++ build.fsx
@@ -0,0 +1,45 @@
+#r "paket: groupref FakeBuild //"
+
+#load ".fake/build.fsx/intellisense.fsx"
+
+open Fake.Core
+open Fake.DotNet
+open Fake.IO
+open Fake.Core.TargetOperators
+
+let [<Literal>] ProjectFilePath = "src/aver/aver.fsproj"
+let [<Literal>] TestProjectPath = "test/aver.test/aver.test.fsproj"
+let [<Literal>] ProjectDirPath = "src/aver/"
+
+Target.create "Clean" (fun _ ->
+    Shell.deleteDirs  [ProjectDirPath + "/bin"; ProjectDirPath + "/obj"]
+)
+
+Target.create "Restore" <| fun _ ->
+    let setParams (defaults: DotNet.RestoreOptions) =
+        { defaults with
+            NoCache = true }
+
+    DotNet.restore setParams ProjectFilePath
+
+Target.create "Build" <| fun _ ->
+    let setParams (defaults: DotNet.BuildOptions) =
+        { defaults with
+           Configuration = DotNet.BuildConfiguration.Release
+           OutputPath = Some "bin/Release" }
+
+    DotNet.build setParams ProjectFilePath
+
+Target.create "Test" <| fun _ ->
+    let setParams (defaults: DotNet.TestOptions) =
+        { defaults with
+            NoRestore = true }
+
+    DotNet.test setParams TestProjectPath
+
+"Clean"
+    ==> "Restore"
+    ==> "Build"
+    ==> "Test"
+
+Target.runOrDefault "Test"
